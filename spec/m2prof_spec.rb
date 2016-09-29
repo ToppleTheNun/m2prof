91430f2ddd06384ffa766ffe65c6931ae6ee3814
Richard Harrah <richard.harrah2@anthem.com>
2016-09-29 16:04:59 -0400

    cleaning up M2Prof spec

--- spec/m2prof_spec.rb
+++ spec/m2prof_spec.rb
@@ -4,27 +4,29 @@ RSpec.describe M2Prof do
   it { should be_const_defined(:VERSION) }
 
   describe '.m2_settings_home' do
-    it { should respond_to :m2_settings_home }
+    it { is_expected.to respond_to :m2_settings_home }
 
-    before(:each) do
+    before(:context) do
       @cached_env = ENV['M2_SETTINGS_HOME']
     end
-    after(:each) do
+    after(:context) do
       ENV['M2_SETTINGS_HOME'] = @cached_env
     end
 
     context 'when M2_SETTINGS_HOME is set' do
-      it "is #{Dir.pwd}/tmp" do
+      before(:each) do
         ENV['M2_SETTINGS_HOME'] = "#{Dir.pwd}/tmp"
-        expect(M2Prof.m2_settings_home).to eq "#{Dir.pwd}/tmp"
       end
+      subject { M2Prof.m2_settings_home }
+      it { is_expected.to eq "#{Dir.pwd}/tmp" }
     end
 
     context 'when M2_SETTINGS_HOME is not set' do
-      it "is #{Dir.home}/.m2" do
+      before(:each) do
         ENV['M2_SETTINGS_HOME'] = nil
-        expect(M2Prof.m2_settings_home).to eq "#{Dir.home}/.m2"
       end
+      subject { M2Prof.m2_settings_home }
+      it { is_expected.to eq "#{Dir.home}/.m2" }
     end
   end
 end
